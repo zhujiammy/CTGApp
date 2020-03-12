37ba71329f123ea093b3e2f56fbb457df3d629dc
zhujiammy <945529210@qq.com>
2020-03-12 17:23:29 +0800

    消息界面

--- app/src/main/java/com/c/ctgapp/ui/news/NewsFragment.java
+++ app/src/main/java/com/c/ctgapp/ui/news/NewsFragment.java
@@ -11,20 +11,35 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.c.ctgapp.Adapter.NewsFragmentAdapter;
 import com.c.ctgapp.R;
+import com.c.ctgapp.Tools.RecyclerViewEmptySupport;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 //消息
 public class NewsFragment extends Fragment {
 
     private NewsViewMode newsViewMode;
+    private NewsFragmentAdapter adapter;
+    @BindView(R.id.recyclerView)
+    RecyclerViewEmptySupport recyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         newsViewMode =
                 ViewModelProviders.of(this).get(NewsViewMode.class);
         View root = inflater.inflate(R.layout.fragment_news, container, false);
+        ButterKnife.bind(this,root);
+        adapter = new NewsFragmentAdapter(getActivity());
+        mLinearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLinearLayoutManager);
+        recyclerView.setAdapter(adapter);
 
         newsViewMode.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
